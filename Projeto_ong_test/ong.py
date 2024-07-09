99c5e245aba110e06ca24eac5310f4886fd6735d
LucasDiasPereira <115173683+LucasDiasPereira@users.noreply.github.com>
2024-07-08 20:44:16 -0400

    Update ong.py

--- Projeto_ong_test/ong.py
+++ Projeto_ong_test/ong.py
@@ -1,12 +1,7 @@
 class ONG():
-    def __init__(self):
+    def __init__(self, nome):
+        self.nome = nome
         self.projetos = []
-
-    def criacao_ong(nome):
-        ong = {
-        'nome':nome,         
-        }
-        return ong
 
     def adicionar_projeto(self, projeto):
         self.projeto = projeto
@@ -16,20 +11,37 @@ class ONG():
     def listar_projetos(self):
         print(self.projeto)
 
-    def buscar_projeto(self, nome=None):
+    def buscar_projeto(self, nome):
         projeto_nome = input("Pesquisa de ong por nome: ")
         for projeto in self.projeto:
             if projeto_nome == nome:
                 return projeto
-            
-class Gerenciador():
-    ongs = []
-    def __init__(self):
-        self.ong = ONG()
-    def criacao_ong(self,ong):
-        self.ong = ong
-        self.ongs.append(ong)
+        return None    
 
+class Projeto():
+    def __init__(self, nome,descricao,responsavel,status):
+        self.nome = nome
+        self.descricao = descricao
+        self.responsavel = responsavel
+        self.status = status
+
+class Gerenciador():
+    def __init__(self):
+        self.ongs = []
+    def criacao_ong(self,nome):
+        self.ongs.append(ONG(nome))
+    def listar_ongs(self):
+        for i in range(0,len(gerenciador.ongs)):
+            print(f"{i+1} - {gerenciador.ongs[i].nome}")
+    def buscar_ongs(self):
+        ong_busca = input("Digite o nome da ONG que deseja buscar: ").capitalize()
+        for ong in self.ongs:
+            if ong_busca == ong.nome:
+                print("Ong selecionada")
+                return ong
+            else:
+                print("Ong não encontrada")
+        
 gerenciador = Gerenciador()
 
 
@@ -43,10 +55,31 @@ while True:
     print("5 - Sair\n")
     opcao = int(input(""))
     if opcao == 1:
-        nome = input("Digite o nome da ONG: ")
-        ong = ONG.criacao_ong(nome)
+        nome = input("Digite o nome da ONG: ").capitalize()
+        gerenciador.criacao_ong(nome)
         print("Ong cadastrada com sucesso!")
+
     elif opcao == 2:
-        projeto = input("Adicione um projeto a ONG: ")
-        ong = ONG.adicionar_projeto(projeto)
-        print("Projeto cadastrado com sucesso!")
+        print("Em qual ONG vamos cadastrar o projeto? ")
+        for ong in gerenciador.ongs:
+            print(f"{gerenciador.ong + 1} - {ong.nome}")
+            ong_do_projeto = int(input("\nSelecione: \n")) - 1
+            if ong_do_projeto < 0 or ong_do_projeto > len(gerenciador.ongs):
+                print("Ong não encontrada")
+            else:
+                projeto = input("Digite o nome do projeto: ")
+                ong = ONG.adicionar_projeto(projeto)
+                print("Projeto cadastrado com sucesso!")
+
+    elif opcao == 3:
+        gerenciador.listar_ongs()
+                
+    elif opcao == 4:
+       ong = gerenciador.buscar_ongs() 
+       print(ong.nome) 
+        
+    elif opcao == 5:
+        print("Saindo do sistema...")
+        break
+    else:
+        print("Opção inválida, selecione uma das opções do menu.")
