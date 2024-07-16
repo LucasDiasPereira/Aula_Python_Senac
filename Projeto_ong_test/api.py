cc898759051c2f2c53860860eba1a9d0a72a51f6
LucasDiasPereira <115173683+LucasDiasPereira@users.noreply.github.com>
2024-07-15 20:23:27 -0400

    Add files via upload

---
+++ Projeto_ong_test/api.py
@@ -0,0 +1,28 @@
+import http.client
+import json
+def print_json(object):
+    if type(object) is list:
+        for json_ in object:
+            for key in json_.keys():
+                print(f"{key} : {json_[key]}")
+    elif type(object) is dict:
+        for key in object.keys():
+            print(f"{key} : {object[key]}")
+
+
+def api(url):
+    http_request = http.client.HTTPSConnection('brasilapi.com.br')
+    http_request.request('GET', url)
+    response = http_request.getresponse()
+    resposta_byte = response.read()
+    print(resposta_byte)
+    json_ = json.loads(resposta_byte)
+    return json_
+
+while True:
+    # ano = input('Ano: ')
+    # json_feriado = api(f"/api/feriados/v1/{ano}")
+    # print_json(json_feriado)
+    cep = input("Digite seu CEP: ")
+    json_cep = api(f"/api/cep/v1/{cep}")
+    print_json(json_cep)
