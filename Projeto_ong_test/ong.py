a2835531f82940d086549f835deb44bfd98bf727
LucasDiasPereira <115173683+LucasDiasPereira@users.noreply.github.com>
2024-07-17 21:42:09 -0400

    Update ong.py

--- Projeto_ong_test/ong.py
+++ Projeto_ong_test/ong.py
@@ -1,55 +1,83 @@
-import http.client
-import json
+ongs: list['ONG'] = []
 
-class ONG:
+class ONG():
     def __init__(self, nome):
         self.nome = nome.capitalize()
-        self.projetos = []
+        self.projetos: list['Projeto'] = []
 
-class Projeto:
+    def listar_projetos(self):
+        print(self.projetos)
+
+    def buscar_projeto(self, nome):
+        projeto_nome = input("Pesquisa de ong por nome: ").capitalize()
+        for projeto in self.projetos:
+            if projeto_nome == nome:
+                return projeto
+        return None
+
+
+class Projeto():
     def __init__(self, nome, descricao, responsavel, status):
         self.nome = nome.capitalize()
         self.descricao = descricao
         self.responsavel = responsavel
         self.status = status
 
-class Gerenciado:
-    def __init__(self):
-        self.ongs = []
-        self.api()
 
-    def api(self):
-        http_request = http.client.HTTPSConnection('api.jsonbin.io')
-        http_request.request('GET', '/v3/b/66985766acd3cb34a8679cdf')
-        response = http_request.getresponse()
-        resposta_byte = response.read()
-        json_ = json.loads(resposta_byte)['record']
-        
-        for ong in json_['ongs']:
-            ong_ = ONG(ong['nome'])
-            for projeto in ong['projetos']:
-                ong_.projetos(Projeto(projeto['nome'], projeto['descricao'], projeto['responsavel'], projeto['status']))
-                ong_.adicionar_projeto(projeto)
-            self.adicionar_ong(ong_)
+def listar_ongs():
+    for i, ong in enumerate(ongs, 1):
+        print(f"{i} - {ong.nome}")
+
+
+def buscar_ongs():
+    ong_busca = input("Digite o nome da ONG que deseja buscar: ").capitalize()
+    for ong in ongs:
+        if ong_busca == ong.nome:
+            print("Ong selecionada")
+            return ong
+        else:
+            print("Ong não encontrada")
     
-    def adicionar_ong(self, ong):
-        self.ongs.append(ong)
 
-    def adiconar_projeto(self,nome, descricao, responsavel, status):
-        self.nome = nome.capitalize()
-        self.descricao = descricao
-        self.responsavel = responsavel
-        self.status = status
+while True:
+    print("\n\nBem vindo ao menu, escolha uma das opções abaixo: \n\n")
+    print("1 - Cadastrar ONG\n")
+    print("2 - Cadastrar Projeto\n")
+    print("3 - Listar ONGs\n")
+    print("4 - Buscar Ong por nome\n")
+    print("5 - Sair\n")
+    opcao = int(input(""))
 
-    def printa(self):
-        for ong in self.ongs:
-            print(f"ONG: {ong.nome}")
-            for projeto in ong.projetos:
-                print(f"  - {projeto.nome}")
-                print(f"    - {projeto.descricao}")
-                print(f"    - {projeto.responsavel}")
-                print(f"    - {projeto.status}")
+    if opcao == 1:
+        nome = input("Digite o nome da ONG: ").capitalize()
+        ongs.append(ONG(nome))
+        print("Ong cadastrada com sucesso!")
+
+    elif opcao == 2:
+        print("Em qual ONG vamos cadastrar o projeto? ")
+        listar_ongs()
+        ong_do_projeto = int(input("\nSelecione: \n")) - 1
+        if ong_do_projeto < 0 or ong_do_projeto > len(ongs):
+            print("Ong não encontrada")
+        else:
+            nome = input("Digite o nome do projeto: ")
+            descricao = input("Digite a descrição do projeto: ")
+            responsavel = input("Digite o nome do responsável pelo projeto: ")
+            status = input("Digite o status do projeto: ")
+            ong = ongs[ong_do_projeto]
+            ong.projetos.append(Projeto(nome, descricao, responsavel, status))
+            print(f"O projeto {nome} foi cadastrado com sucesso!")
             
 
-gerenciado = Gerenciado()
-gerenciado.printa()
+    elif opcao == 3:
+        listar_ongs()
+
+    elif opcao == 4:
+       ong = buscar_ongs() 
+       print(ong.nome)
+        
+    elif opcao == 5:
+        print("Saindo do sistema...")
+        break
+    else:
+        print("Opção inválida, selecione uma das opções do menu.")
