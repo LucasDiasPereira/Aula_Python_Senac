63435c59ee522dde97a098dc4778657609057055
LucasDiasPereira <115173683+LucasDiasPereira@users.noreply.github.com>
2024-08-05 20:58:42 -0400

    Delete Teorico directory

--- Teorico/view.py
+++
@@ -1,7 +0,0 @@
-def menuzinho():
-        print("\n\nEscolha uma opção do menu:")
-        print("1. Cadastrar Humano")
-        print("2. Cadastrar Cachorro")
-        print("3. Listar Humanos")
-        print("4. Listar Cachorros")
-        print("5. Sair\n")
