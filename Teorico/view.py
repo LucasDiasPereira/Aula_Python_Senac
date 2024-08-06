7cf2b23ac3589550ec48f229287548b709fa301f
LucasDiasPereira <115173683+LucasDiasPereira@users.noreply.github.com>
2024-08-05 21:11:50 -0400

    Add files via upload

---
+++ Teorico/view.py
@@ -0,0 +1,8 @@
+def menuzinho():
+        print("\n\nEscolha uma opção do menu:")
+        print("1. Cadastrar Humano")
+        print("2. Cadastrar Cachorro")
+        print("3. Listar Humanos")
+        print("4. Listar Cachorros")
+        print("5. Sair\n")
+        return int(input("Digite uma opção: "))
